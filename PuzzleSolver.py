6fa1962d12758305914d4b0aa5d908aa6260c581
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2017-12-27 02:00:36 +0900

    軽微な修正

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -9,8 +9,12 @@ class PuzzleSolver:
 	def appendPiece(self, piece: Piece):
 		self._pieces.append(piece)
 
-	# このメソッドの処理を並列化(あるいは平行化)して解の導出を速める
+	# このメソッドの処理を並列化(或は並行化)して解の導出を速める
 	# アルゴリズム次第ですね↑
+	#
+	# 並列化: マルチスレッドで処理を行う事
+	# 並行化: マルチプロセスで処理を行う事。プロセス単位の分離により、マルチコアで処理可能。
+	# 参考URL >> https://qiita.com/castaneai/items/9cc33817419896667f34
 	def solve(self) -> list:
 		return self._pieces # 取敢えず鸚鵡返し
 

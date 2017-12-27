b6a55f45b14195c784fd428b1b644b59d3b7b37e
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2017-12-28 00:55:33 +0900

    諸諸
    眠みによるつらみが錯誤の原因であると推定される

--- Piece.py
+++ Piece.py
@@ -1,5 +1,6 @@
 ﻿class Piece:
 	_vertexes: list # 頂点((int, int)型)を格納したリスト
+	# ここに角度のデータを格納した変数等
 
 	def __init__(self, vertexes: list):
 		if len(vertexes) < 3:
@@ -9,11 +10,31 @@
 
 		self._vertexes = vertexes
 
+		# ここで角度の算出等
+
 	def new(vertexes: list):
 		return Piece(vertexes)
 
 	def getVertexes(self) -> list:
 		return self._vertexes
+
+	# 片を回転させる
+	def rotate(self):
+		pass
+
+	# 片を裏返す
+	def flip(self):
+		pass
+
+	# 片を移動させる
+	def move(self):
+		pass
+
+	def isOnGrid(self) -> bool:
+		pass
+
+	def isInAngle(self) -> bool:
+		pass
 
 
 class PieceError(Exception):
@@ -27,10 +48,7 @@ class PieceError(Exception):
 
 
 class PieceUtils:
+	# ２片を結合する
 	@staticmethod
 	def marge(x: Piece, y: Piece) -> Piece:
-		pass
-
-	@staticmethod
-	def rotate(x: Piece) -> Piece:
 		pass

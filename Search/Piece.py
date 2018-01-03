dd007cafd57e8f1865a77f9cfd5cf6131986d0f2
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-01-03 23:03:38 +0900

    エラーの条件変更

--- Search/Piece.py
+++ Search/Piece.py
@@ -11,9 +11,9 @@ class Piece (Figure):
 			vec_back = np.array(vertexes[(i - 1 + len(vertexes)) % len(vertexes)]) - np.array(vertex)
 			inner_product = np.dot(vec_front,vec_back)
 			rad = np.arccos(float(inner_product) / (np.linalg.norm(vec_front) * np.linalg.norm(vec_back)))
-			if rad == 0.0:
-				raise FigureError("角度が0度")
 			angle = np.rad2deg(rad)
+			if angle < 1.0:
+				raise FigureError("角度が1度未満")
 			#if judge.is_in_angle(self,vertex)==false: #外角だった場合
 			#	angle = 360.0 - angle
 			self._angles.append(angle)

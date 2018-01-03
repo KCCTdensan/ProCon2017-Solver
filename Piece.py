5dcea5ac3c7f104ee08afe8cce242b3ca7bc28ef
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-01-03 17:12:05 +0900

    ちょっと修正

--- Piece.py
+++ Piece.py
@@ -1,9 +1,8 @@
-﻿import math
-import numpy as np
+﻿import numpy as np
 
 class Piece:
 	_vertexes: list # 頂点((int, int)型)を格納したリスト
-	_angles: list # 角度を格納したリスト
+	_angles = [] # 角度を格納したリスト
 
 	def __init__(self, vertexes: list):
 		if len(vertexes) < 3:
@@ -12,23 +11,24 @@ class Piece:
 		self._vertexes = vertexes
 
 		# ここで角度の算出等
-		angles=[]
 		for i,vertex in enumerate(vertexes):
 			vec_front = np.array(vertexes[(i + 1) % len(vertexes)]) - np.array(vertex)
 			vec_back = np.array(vertexes[(i - 1 + len(vertexes)) % len(vertexes)]) - np.array(vertex)
 			inner_product = np.dot(vec_front,vec_back)
 			rad = np.arccos(float(inner_product) / (np.linalg.norm(vec_front) * np.linalg.norm(vec_back)))
 			angle = np.rad2deg(rad)
-			#if judge.is_in_angle(self,vertex)==false:
-			#	angle = 2 * math.pi - angle
-			angles.append(angle)
-		self._angles=angles
+			#if judge.is_in_angle(self,vertex)==false: #外角だった場合
+			#	angle = 360.0 - angle
+			self._angles.append(angle)
 
 	def new(vertexes: list):
 		return Piece(vertexes)
 
 	def getVertexes(self) -> list:
 		return self._vertexes
+
+	def getAngles(self) -> list:
+		return self._angles
 
 	# 片を回転させる
 	def rotate(self):

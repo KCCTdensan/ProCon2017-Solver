be9a8ca59a61f0c7c5e79b27fb7c1b82f489c462
wannashraf <r115333@g.kobe-kosen.ac.jp>
2018-01-17 01:06:37 +0900

    rotate実装

--- Search/Piece.py
+++ Search/Piece.py
@@ -12,8 +12,21 @@ class Piece (Figure):
 		return Piece(vertexes)
 
 	# 片を回転させる
-	def rotate(self):
-		pass
+	def rotate(self,self_vertex1,self_vertex2,another_vertex1,another_vertex2):
+		self_vertexes = self.getVertexes()
+        # ベクトルにする
+		vecter_self = self.vertexes[self_vertex2]-self.vertexes[self_vertex1]
+		vecter_another = another.vertexes[another_vertex2]-another.vertexes[another_vertex1]
+        # 内積やなんやらで角度を出す
+		rotate_angle = math.acos(numpy.dot(vecter_self,vecter_another) / (numpy.linalg.norm(vecter_self) * numpy.linalg.norm(vecter_another)))
+        # ↓回転行列
+		rotate_matrix = numpy.matrix([
+            [numpy.cos(rotate_angle) , -numpy.sin(rotate_angle)],
+            [numpy.sin(rotate_angle) , numpy.cos(rotate_angle)]
+            ])
+        # 座標変更
+		shifted_self_vertexes=numpy.squeeze(numpy.asarray((rotate_matrix*self_vertexes.T).T))
+		return shifted_self_vertexes
 
 	# 片を裏返す
 	def flip(self):

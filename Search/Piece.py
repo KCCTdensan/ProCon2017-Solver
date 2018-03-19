dc5b95a93cd89d801a1c2626053396217ad755d8
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-03-20 01:39:14 +0900

    角度計算の修正 結果は同じだが、一部変更

--- Search/Piece.py
+++ Search/Piece.py
@@ -3,11 +3,11 @@ import numpy as np
 
 class Piece (Figure):
 	def __init__(self, vertexes: list):        
-		#複素数1から複素数2の原点を中心とした角
+		#複素数1から複素数2の原点を中心とした角(deg)
 		def angleBetweenComplexes(complex1, complex2):
-			return np.angle(complex2/complex1)
+			return (np.rad2deg(np.angle(complex2/complex1)) + 360) % 360
 
-		#ベクトル1からベクトル2の原点を中心とした角
+		#ベクトル1からベクトル2の原点を中心とした角(deg)
 		def angleBetweenVectors(vector1, vector2):
 			return angleBetweenComplexes(vector1[0] + 1j*vector1[1], vector2[0] + 1j*vector2[1])
 
@@ -16,7 +16,7 @@ class Piece (Figure):
 		self._angles = []
 		numVertexes = len(vertexes)
 		for i in range(0, numVertexes):
-			self._angles.append((np.rad2deg(angleBetweenVectors(np.array(vertexes[(i - 1 + numVertexes) % numVertexes]) - np.array(vertexes[i]), np.array(vertexes[(i + 1) % numVertexes]) - np.array(vertexes[i]))) + 360) % 360)
+			self._angles.append(angleBetweenVectors(np.array(vertexes[(i - 1 + numVertexes) % numVertexes]) - np.array(vertexes[i]), np.array(vertexes[(i + 1) % numVertexes]) - np.array(vertexes[i])))
 
 	def new(vertexes: list):
 		return Piece(np.array(vertexes))

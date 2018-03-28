d3e283448ebdaa3b3210797b1b17a3202dc1104a
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-03-28 23:02:07 +0900

    おおまかな形

--- Search/PuzzleSolver.py
+++ Search/PuzzleSolver.py
@@ -5,6 +5,8 @@ class PuzzleSolver:
 	_frame: Frame # 枠
 	_pieces: list = [] #片のリスト
 
+	MergedPieces = [] #mergeしたpieceのリスト
+
 	def new():
 		return PuzzleSolver()
 
@@ -15,7 +17,18 @@ class PuzzleSolver:
 		self._pieces.append(piece)
 
 	def solve(self) -> list:
-		return self._pieces # 取敢えず、蓄積した片のリストを返すだけ
+		if len(self._pieces) == 0:#完成
+			return True
+		for piece in self._pieces:
+			for i in range(len(piece._vertexes)):#pieceごとにすべての辺の組合わせをみる
+				if Judge.isMarge(self._frame, piece, piece.vertexes[i], piece.vertexes[(i+1) % len(piece.vertexes)]):
+					self._frame.marge(piece, piece.vertexes[i], piece.vertexes[(i+1) % len(piece.vertexes)])
+					#self._vertexesからmergeしたpieceを削除
+					PuzzleSolver.MergedPieces += piece #pieceがmergeされる際の座標をスタックにプッシュ
+					Solver = PuzzleSolver.new(self._frame, self._pieces)
+					return PuzzleSolver.solve()
+		PuzzleSolver.MergedPieces.pop() #スタックからポップ
+		return False #このルートは完成までたどり着けない
 
 
 class PuzzleSolverUtils:

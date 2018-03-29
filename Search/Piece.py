b4fa3da8d45da2ac94616c127a6a8aeb8fe0103f
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-03-29 16:26:12 +0900

    書き方の修正と関数の移動

--- Search/Piece.py
+++ Search/Piece.py
@@ -3,20 +3,20 @@ import numpy as np
 
 class Piece (Figure):
 	def __init__(self, vertexes: list):        
-		#複素数1から複素数2の原点を中心とした角(deg)
-		def angleBetweenComplexes(complex1, complex2):
-			return (np.angle(complex2/complex1, deg = True) + 360) % 360
-
-		#ベクトル1からベクトル2の原点を中心とした角(deg)
-		def angleBetweenVectors(vector1, vector2):
-			return angleBetweenComplexes(vector1[0] + 1j*vector1[1], vector2[0] + 1j*vector2[1])
-
 		super().__init__(vertexes)
 		# 角度群の算出
 		self._angles = []
 		numVertexes = len(vertexes)
 		for i in range(0, numVertexes):
-			self._angles.append(angleBetweenVectors(np.array(vertexes[(i - 1 + numVertexes) % numVertexes]) - np.array(vertexes[i]), np.array(vertexes[(i + 1) % numVertexes]) - np.array(vertexes[i])))
+			self._angles.append(angleBetweenVectors(np.array(vertexes[i - 1]) - np.array(vertexes[i]), np.array(vertexes[(i + 1) % numVertexes]) - np.array(vertexes[i])))
+
+	# 複素数1から複素数2の原点を中心とした角(deg)
+	def angleBetweenComplexes(complex1, complex2):
+		return (np.angle(complex2/complex1, deg = True) + 360) % 360
+
+	# ベクトル1からベクトル2の原点を中心とした角(deg)
+	def angleBetweenVectors(vector1, vector2):
+		return angleBetweenComplexes(vector1[0] + 1j*vector1[1], vector2[0] + 1j*vector2[1])
 
 	def new(vertexes: list):
 		return Piece(np.array(vertexes))
